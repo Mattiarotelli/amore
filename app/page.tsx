e97e7cd4b51e257dc994f60e6da777da294fea68
Mattiarotelli <m.rotelli06@gmail.com>
2024-02-20 20:44:48 +0100

    style: :art: Little grafic changes
    i had Insert a "compents.json" and compents folder, added an Avatar component and an "h1"
    
    I can't change the image of the avatar

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,46 @@
 "use client";
 import { useState, useEffect } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 export default function Home() {
-  const [cont, setCont] = useState(0);
+  const [cont, setCont] = useState(2348);
   function x() {
-    if(cont>= 50){
-      return "🤍"
-    }else{
-      return cont
+    if (cont >= 10000) {
+      return "🤍";
+    } else {
+      return cont;
     }
   }
   useEffect(() => {
+    const interval = setInterval(
+      () => {
+        //delay di mezzo secondo
+        console.log(cont);
+        if (cont < 10000) {
+          setCont(cont + 1); //incrementa
+        } else {
+          clearInterval(interval);
+        }
+      },
+      0,
+      5
+    );
 
-    const interval = setInterval(() => {    //delay di mezzo secondo 
-      console.log(cont)
-      if(cont<50){
-        setCont(cont+1)     //incrementa
-      }else{
-        clearInterval(interval)
-      }
-       
-    }, 10);
-  
-    return () => clearInterval(interval);     //perche si 
+    return () => clearInterval(interval); //perche si
   }, [cont]);
-  return <>{x()}</>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-row gap-3 text-center">
+        <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png"/>
+        <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      mattiarotelli  
+      </div>
+
+      <div className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 justify-center text-center flex flew-row">
+        Quanto ti amo: {x()}
+      </div>
+    </div>
+  );
 }
