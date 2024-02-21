d0d8a5f0084e8242916ee5884b52d83e593e0386
KovD3v <tommaso.coviello2@gmail.com>
2024-02-21 13:28:13 +0100

    refactor: :zap: Optimize counter and render
    Add to the counter  a random number and not rerender the same number using the hook useCallback

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,46 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export default function Home() {
-  const [cont, setCont] = useState(2348);
-  function x() {
+  const [cont, setCont] = useState(0);
+  
+  const x = useCallback(() => {
     if (cont >= 10000) {
       return "🤍";
     } else {
       return cont;
     }
-  }
+  }, [cont]);
+
+  const getRand = () => {
+    return Math.floor(Math.random() * 10);
+  };
+
   useEffect(() => {
     const interval = setInterval(
       () => {
-        //delay di mezzo secondo
         console.log(cont);
         if (cont < 10000) {
-          setCont(cont + 1); //incrementa
+          setCont(cont + getRand());
         } else {
           clearInterval(interval);
         }
       },
-      0,
-      5
+      1
     );
 
-    return () => clearInterval(interval); //perche si
+    return () => clearInterval(interval);
   }, [cont]);
+
   return (
     <div className="flex flex-col">
       <div className="flex flex-row gap-3 text-center">
         <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png"/>
+        <AvatarImage src="/pfp.jpg"/>
         <AvatarFallback>CN</AvatarFallback>
         </Avatar>
-      mattiarotelli  
+        mattiarotelli
       </div>
 
       <div className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 justify-center text-center flex flew-row">
